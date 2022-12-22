0c7fa79bf2118df0185ca0ae6a30e7ed82f21466
CastellucciLuca <castellucciluca98@gmail.com>
2022-12-22 10:11:33 +0100

    first-snack

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/**
+Creare due div; uno conterrà numeri dispari di colore rosso e l'altro conterrà numeri pari in verde.
+Ad ogni click di un bottone chiedere all'API un numero, se è dispari metterlo nel blocco dispari, e se è pari in quello pari.
+**/
+const {createApp} = Vue;
+createApp({
+	data() {
+		return{
+        evenNumber : [],
+        oddNumber : [],
+		}
+	},
+    methods: {
+        generateRandomNumber(){
+                axios.get('https://flynn.boolean.careers/exercises/api/array/integers?min=1&max=100&items=1')
+                .then((response) => {
+                    const result = response.data.response[0];
+                    console.log(result);
+                    if (result % 2 == 0){
+                        this.evenNumber.push(result);
+                    } else {
+                        this.oddNumber.push(result);
+                    }
+                });
+            }
+        },
+}).mount ('#app')
